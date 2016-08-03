8fe01e268cce8dfd99ceb18cad51e41e5fef369c
eduardrg <eduardrg@uw.edu>
2016-08-03 16:49:11 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php
 
 	$service_url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=8476C3AFEFAAAB4F9E96A21BB1F39D88&steamids=76561198044658071';
+	
 	$curl = curl_init($service_url);
+
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	$curl_response = curl_exec($curl);
+
 	if ($curl_response === false) {
 	    $info = curl_getinfo($curl);
 	    curl_close($curl);
 	    die('error occured during curl exec. Additional info: ' . var_export($info));
 	}
+
 	curl_close($curl);
+
 	$decoded = json_decode($curl_response);
+
 	if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
 	    die('error occured: ' . $decoded->response->errormessage);
 	}
@@ -18,7 +24,6 @@
 
 	$state = strrpos(string $decoded, string 'personastate') + 17;
 	// var_export($decoded->response);
-
 
 	switch ($state) {
 	    case 0:
@@ -44,4 +49,6 @@
 	        break;
 	    default:
 	        echo "unknown";
+    }
+
 }
