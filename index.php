b91eb5a4d2314f903865f26eef267321f4560a51
eduardrg <eduardrg@uw.edu>
2016-08-03 16:46:10 -0700

    Update and rename test.php to index.php

--- index.php
+++ index.php
@@ -15,6 +15,33 @@
 	    die('error occured: ' . $decoded->response->errormessage);
 	}
 	echo 'Response ok!';
-	var_export($decoded->response);
 
-?>
+	$state = strrpos(string $decoded, string 'personastate') + 17;
+	// var_export($decoded->response);
+
+
+	switch ($state) {
+	    case 0:
+	        echo "0 - Offline";
+	        break;
+	    case 1:
+	        echo "1 - Online";
+	        break;
+	    case 2:
+	        echo "2 - Busy";
+	        break;
+	    case 3:
+	        echo "3 - Away";
+	        break;
+	    case 4:
+	        echo "4 - Snooze";
+	        break;
+	    case 5:
+	        echo "5 - looking to trade";
+	        break;
+	    case 6:
+	        echo "6 - looking to play";
+	        break;
+	    default:
+	        echo "unknown";
+}
